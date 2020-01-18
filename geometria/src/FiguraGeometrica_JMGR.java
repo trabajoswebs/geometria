7352e77db5eed76b1d40e0796253395a30b30a7b
Johan Manuel <Johan Manuel@DESKTOP-I06SU2E>
2020-01-18 22:02:33 +0100

    nuevo método

--- geometria/src/FiguraGeometrica_JMGR.java
+++ geometria/src/FiguraGeometrica_JMGR.java
@@ -19,5 +19,9 @@ public abstract class FiguraGeometrica_JMGR{
 	public void setTipoFigura(String tipoFigura) {
 		this.tipoFigura = tipoFigura;
 	}
+	
+	public double semiPerimetro() {
+		return this.perimetro() / 2;
+	}
 
 }
