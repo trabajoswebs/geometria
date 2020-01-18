3562077e078ff620e39ea7da3ef478c571c7135e
Johan Manuel <Johan Manuel@DESKTOP-I06SU2E>
2020-01-18 22:13:59 +0100

    versión comentada

--- geometria/src/Rectangulo_JMGR.java
+++ geometria/src/Rectangulo_JMGR.java
@@ -1,20 +1,39 @@
 
+/**
+ * Esta clase se encuentra los metodos para crear un rectangulo
+ * @author Johan Manuel Gonzalez Rodriguez
+ * @version 1.2
+ */
 public class Rectangulo_JMGR extends FiguraGeometrica_JMGR{
 	private double l1;
 	private double l2;
 	
-	public Rectangulo_JMGR(String tipoFigura, double lG, double lP) {
-		
+	
+	
+	/** Constructor de la clase
+	 * @param tipoFigura String 
+	 * @param lG double
+	 * @param lP double
+	 */
+	public Rectangulo_JMGR(String tipoFigura, double lG, double lP) {		
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
+	
+	/**
+	 * Metodo que sobreescribe el calculo de un area
+	 * @return double devuelve el valor del area
+	 */
 
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * Metodo que sobreescribe el calculo del perimetro
+	 * @return double devuelve el valor del perimetro
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
