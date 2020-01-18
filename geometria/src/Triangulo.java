99613c00bd5b43f49db01b99fd10d8f5b9c64f13
Johan Manuel <Johan Manuel@DESKTOP-I06SU2E>
2020-01-18 21:43:54 +0100

    Cambio de nombre de variables

--- geometria/src/Triangulo.java
+++ geometria/src/Triangulo.java
@@ -1,26 +1,26 @@
 
 public class Triangulo extends FiguraGeometrica {
-	private double a;
-	private double b;
-	private double c;
+	private double lado1;
+	private double lado2;
+	private double lado3;
 	
 	public Triangulo(String tipoFigura, double lado1, double lado2, double lado3) {
 		super(tipoFigura);
-		a = lado1;
-		b = lado2;
-		c = lado3;
+		lado1 = lado1;
+		lado2 = lado2;
+		lado3 = lado3;
 	}
 
 	@Override
 	public double perimetro() {
-		return a + b + c;
+		return lado1 + lado2 + lado3;
 	}
 
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
 		sp = this.semiPerimetro();
-		return Math.sqrt(sp * (sp-a) * (sp-b) * (sp-c));
+		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
 	
 	public double semiPerimetro() {
